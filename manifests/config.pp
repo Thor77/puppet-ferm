619b61011c2e4b3f5253c08500e24b5c59382017
Thor77 <thor77@thor77.org>
2019-07-30 20:59:20 +0200

    Build rule in template

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,12 +1,10 @@
 # @summary Manage ferm configuration
 class ferm::config {
-  $rules = $ferm::rules.map |$comment, $rule| {
-    ferm::build_rule($rule, $ferm::rule_defaults, $comment)
-  }
   file { $ferm::config_path:
     ensure  => 'present',
     content => epp('ferm/ferm.conf.epp', {
-      rules         => $rules,
+      rules         => $ferm::rules,
+      rule_defaults => $ferm::rule_defaults,
     }),
   }
 }
