9a69409042c73f15cad25d403d547578d0b45006
Thor77 <thor77@thor77.org>
2019-07-31 21:32:54 +0200

    Add osfamily case for config_path

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,11 +3,22 @@ class ferm::params {
   $package_name = 'ferm'
   $package_version = 'installed'
   $service_name = 'ferm'
-  $config_path = '/etc/ferm.conf'
   $rule_defaults = {
     domain => ['ip', 'ip6'],
     table  => 'filter',
     chain  => 'INPUT',
     action => 'ACCEPT',
+  }
+
+  case $::osfamily {
+    'Debian': {
+      $config_path = '/etc/ferm/ferm.conf'
+    }
+    'Archlinux': {
+      $config_path = '/etc/ferm.conf'
+    }
+    default: {
+      notice('This module is not supported on this platform.')
+    }
   }
 }
