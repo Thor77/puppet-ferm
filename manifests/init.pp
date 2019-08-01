2ffea07bceee1d388c20589b7111c70b72118158
Thor77 <thor77@thor77.org>
2019-08-01 20:20:21 +0200

    Add puppet strings documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,10 @@
 # @summary Ferm module init class
+# @param rules [Hash] rules to render in config file
+# @param rule_defaults [Hash] defaults for rule, will be merged with each rule
+# @param package_name [String] name of the ferm package
+# @param package_version [String] target version/state of the ferm package resource
+# @param service_name [String] name of the ferm service
+# @param config_path [String] path to ferm config file
 class ferm (
   Hash $rules = {},
   Hash $rule_defaults = $ferm::params::rule_defaults,
